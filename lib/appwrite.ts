47cd426433a129fe1d8a8901ec382c8ae9a2990f
Queñano Jhon Marcellino <okayda999@gmail.com>
2024-11-08 19:25:07 -0600

    Add appwrite create admin client

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -4,7 +4,7 @@ import "server-only";
 
 import { cookies } from "next/headers";
 
-import { Client, Account, Databases } from "node-appwrite";
+import { Client, Account, Users, Databases } from "node-appwrite";
 
 import { AUTH_COOKIE } from "@/features/auth/constants";
 
@@ -28,6 +28,23 @@ export async function createSessionClient() {
 
     get databases() {
       return new Databases(client);
+    },
+  };
+}
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setKey(process.env.NEXT_APPWRITE_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+
+    get users() {
+      return new Users(client);
     },
   };
 }
